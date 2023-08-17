3c1888d46e4261d3e0b03b41ca971ced83d93243
MatheLover <rockongoodtimes@yahoo.com>
2023-08-17 12:05:51 +0800

    specify keras and tensorflow version

--- R/Install_Py_Packages_v2.R
+++ R/Install_Py_Packages_v2.R
@@ -13,7 +13,7 @@
 #' @examples Install_Py_Packages_v2("conda",envname="~/Documents/conda_test")
 Install_Py_Packages_v2 <- function(method="default",envname="my_env", python=NULL, envname_conda="my_conda_env"){
   # package vector
-  pac_c <- c("joblib","numpy","pandas","scikit-learn","pillow","keras","keras_applications","keras-vggface","tensorflow")
+  pac_c <- c("joblib","numpy","pandas","scikit-learn","pillow","keras==2.9.0","keras_applications","keras-vggface","tensorflow==2.9.1")
 
   if(method == "default"){
     Install_Default(pac_c)
